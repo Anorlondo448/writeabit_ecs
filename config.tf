8dd6025b885c962f3b2da06ee17fe0c2fa5817d5
Anorlondo448 <kanaend0605@gmail.com>
2019-02-26 23:42:27 +0900

    terraform fmt

--- config.tf
+++ config.tf
@@ -5,12 +5,12 @@
 terraform {
   backend "s3" {
     bucket = "anorlondo448-writeabit-for-ecs"
-    key = "terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 provider "aws" {
   version = "~> 1.60"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
