36103dc9849696c0d28436e49ecca39da48c2cb0
Anorlondo448 <kanaend0605@gmail.com>
2019-03-01 23:15:05 +0900

    merge s3 bucket policy & document

--- ci/s3_bucket_policy.tf
+++ ci/s3_bucket_policy.tf
@@ -2,6 +2,44 @@
 #
 # S3 Bucket Policy for artifact
 #
+data "aws_iam_policy_document" "s3-bucket-policy-artifact" {
+  statement {
+    sid       = "DenyUnEncryptedObjectsUpload"
+    effect    = "Deny"
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.artifact.arn}/*"]
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      values   = ["aws:kms"]
+      variable = "s3:x-amz-server-side-encryption"
+    }
+  }
+
+  statement {
+    sid       = "DenyInsecureConnections"
+    effect    = "Deny"
+    actions   = ["s3:*"]
+    resources = ["${aws_s3_bucket.artifact.arn}/*"]
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    condition {
+      test     = "Bool"
+      values   = ["false"]
+      variable = "aws:SecureTransport"
+    }
+  }
+}
+
 resource "aws_s3_bucket_policy" "artifact" {
   bucket = "${aws_s3_bucket.artifact.id}"
 
